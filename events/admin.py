eb4f41c74054d73b3012e474b6e4c8b71082df76
tempog0 <tempforall2024@gmail.com>
2024-06-07 21:08:17 +0545

    database

--- events/admin.py
+++ events/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Venue
+from .models import MyClubUser
+from .models import Event
+
+admin.site.register(Venue)
+admin.site.register(MyClubUser)
+admin.site.register(Event)
--- events/models.py
+++ events/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
 # Create your models here.
+class Venue(models.Model):
+    name=models.CharField(max_length=100)
+    address=models.DateField
+    zip_code=models.CharField(max_length=100)
+    web=models.URLField('Website Address')
+    email=models.EmailField('Email Address')
+
+    def __str__(self):
+        return self.name
+
+class MyClubUser(models.Model):
+    first_name=models.CharField(max_length=200)
+    last_name=models.CharField(max_length=100)
+    email=models.EmailField('User Address')
+
+    def __str__(self):
+        return self.first_name + ' ' + self.last_name
+
+
+class Event(models.Model):
+    name=models.CharField(max_length=100)
+    venue=models.ForeignKey(Venue,blank=True,null=True,on_delete=models.CASCADE)
+    manager=models.CharField(max_length=100)
+    description=models.TextField(blank=True)
+    attendes=models.ManyToManyField(MyClubUser,blank=True)
+
+
+    def __str__(self):
+        return self.name
